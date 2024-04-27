79ac1f6dd5e473253eb4886aa52aec14231dee80
Felipe-heide <felipe.heide7@gmail.com>
2024-04-27 00:15:50 -0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const http = require('http');
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
