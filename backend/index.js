15b49d2a96c6daebf1883ddd009efa51eec8b433
Felipe-heide <felipe.heide7@gmail.com>
2024-04-27 00:18:52 -0300

    w

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,8 @@ const http = require('http');
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 4000;
+const HOST = '0.0.0.0';
 
-server.listen(PORT, () => {
+server.listen(PORT, HOST, () => {
   console.log(`Server running on port ${PORT}`);
 });
